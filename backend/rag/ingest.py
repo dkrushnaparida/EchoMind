0dd73db4d1d6e781c6a2926385e311b5d287e3d2
dkrushnaparida <d.krushnaparida2019@gmail.com>
2026-03-10 13:22:26 +0530

    added new code in ingest.py so that it will support multiple file types

--- backend/rag/ingest.py
+++ backend/rag/ingest.py
@@ -1,12 +1,13 @@
 import os
 from pathlib import Path
-
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
-
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    TextLoader,
+    Docx2txtLoader,
+)
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 from backend.core.logger import get_logger
 
 logger = get_logger(__name__)
@@ -18,9 +19,18 @@ VECTOR_DB_PATH = "data/vectordb"
 def load_documents():
     documents = []
 
-    for file in Path(DATA_PATH).glob("*.pdf"):
+    for file in Path(DATA_PATH).glob("*"):
         logger.info(f"Loading file: {file}")
-        loader = PyPDFLoader(str(file))
+        if file.suffix == ".pdf":
+            loader = PyPDFLoader(str(file))
+        elif file.suffix == ".txt":
+            loader = TextLoader(str(file))
+        elif file.suffix == ".docx":
+            loader = Docx2txtLoader(str(file))
+        else:
+            logger.warning(f"Unsupported file type: {file}")
+            continue
+
         docs = loader.load()
         documents.extend(docs)
 
@@ -29,9 +39,7 @@ def load_documents():
 
 def split_documents(documents):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-
     chunks = text_splitter.split_documents(documents)
-
     logger.info(f"Created {len(chunks)} chunks")
 
     return chunks
@@ -39,33 +47,30 @@ def split_documents(documents):
 
 def create_embeddings():
     embeddings = OllamaEmbeddings(model="nomic-embed-text:latest")
-
     return embeddings
 
 
-def create_vector_store(chunks):
-
+def create_vector_store(chunks, collection_name="default"):
+    os.makedirs(VECTOR_DB_PATH, exist_ok=True)
     embeddings = create_embeddings()
     vectordb = Chroma.from_documents(
-        documents=chunks, embedding=embeddings, persist_directory=VECTOR_DB_PATH
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=VECTOR_DB_PATH,
+        collection_name=collection_name,
     )
-
-    logger.info("database created successfully")
-
+    vectordb.persist()
+    logger.info(f"DB created | collection={collection_name}")
     return vectordb
 
 
-def run_ingestion():
+def run_ingestion(collection_name="default"):
     docs = load_documents()
-
     if not docs:
         logger.warning("No documents found")
-        return
-
+        return None
     chunks = split_documents(docs)
-
-    vectordb = create_vector_store(chunks)
-
+    vectordb = create_vector_store(chunks, collection_name=collection_name)
     return vectordb
 
 
